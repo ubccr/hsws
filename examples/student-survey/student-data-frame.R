e801e3e3db5cf8289d5543751034c9e1622e35d5
jsperhac <jsperhac@buffalo.edu>
2014-07-02 09:09:05 -0400

    Updated student data frame with some exercises

--- examples/student-survey/student-data-frame.R
+++ examples/student-survey/student-data-frame.R
@@ -21,3 +21,52 @@ data = data.frame(Height=height,       # Each column gets a name
                   School=school,
                   Hand=handed,                  
                   row.names=rownames)  # provide names for the rows
+
+# --------------------------------
+
+# Exploring bad syntax, and good syntax, for 
+# indexing into data frames:
+
+# select subset of data frame having age < 17.
+# try omitting the comma. What happens?
+data[data$Age<17,]
+
+# This should return a single value. What happens
+# if you omit one of the row or column names? The comma?
+data["Jack D","Hand"]
+
+# what does this do? Why?
+data$"Jack D"
+
+# what does this do? Why?
+data["Height"]
+
+# why doesn't the same type of access work here:
+data["Jack D"]
+
+# any difference between these calls?
+data[,]
+data[]
+data
+
+# try these commands. What do they do?
+summary(data$Height)
+str(data$Height)
+
+# select heights equal to 74 inches.
+# How do you get the whole row instead?
+# What happens (try this last) if you omit one of the =
+# in the ==? Can you guess first?
+data$Height[data$Height==74]
+
+# now what Heights are present in your dataset?
+
+# --------------------------------
+
+# write a function that accomplishes each of these ends:
+#
+# List heights and handedness of students at SJCI
+# How many students are younger than 17?
+# List names of students old enough to drive
+# Convert all heights to centimeters, output a data frame
+#   that contains name, height in cm, school. One inch is 2.54 cm.
