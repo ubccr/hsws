a29f770bcd120cb3d6f68b207ae8c8739e700430
jsperhac <jsperhac@buffalo.edu>
2015-07-01 10:59:39 -0400

    update student dataframe

--- examples/student-survey/student-data-frame.R
+++ examples/student-survey/student-data-frame.R
@@ -1,15 +1,13 @@
 # workshop students data frame example
 
 # construct vectors of data for inclusion in data frame.
-height = c(75, 74, 66, 59, 66, 67, 61,74, 76, 69, 72)      # inches
-units = c("in","in", "in", "in", "in","in", "in","in","in","in","in")
-firstname = c("Steven", "Mike", "Jack", "Sarah", "Thomas", "Jade",
-              "Lorena","Matthew","Markus","Brian", "Jin")   
-lastinitial = c("B", "C", "D", "B", "K", "G", "J", "O","D", "C", "L")   
-school=c("SJCI","WNHS","SJCI", "Nardin", "Nichols", 
-         "NC","Nichols","SJCI","FHS", "WSHS","WSHS")
-age    = c(16, 16, 15, 17, 15, 17, 14, 15, 16, 18,14)      # years
-handed = c("L", "R", "R", "R", "R","R","R","R","R","R","R")   # dominant hand: R=right, L=left
+height = c(68, 67, 65, 72, 71, 68,68 )      # inches
+units = c("in","in", "in", "in", "in","in", "in")
+firstname = c("Rachel", "Molly", "Trevor", "Ethan", "Nicholas","Thomas", "Tyler")   
+lastinitial = c("K","F","P","A", "P", "W","D")   
+school=c("Nardin","MSM","OP","OP", "SJCI","Lake Shore","SJCI")
+age    = c(17, 17,17,17, 15, 16, 14)      # years
+handed = c("R", "R", "R", "R", "L","R","R")   # dominant hand: R=right, L=left
 
 # provide names for the rows (samples)
 rownames = c(paste(firstname,lastinitial))
