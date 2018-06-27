1da7b0579b34c49475e8e69b5aa0d7e99a84b253
Jeanette Sperhac <jsperhac@buffalo.edu>
2018-06-27 09:15:10 -0400

    Update student-data-example.R

--- examples/student-survey/student-data-example.R
+++ examples/student-survey/student-data-example.R
@@ -1,49 +1,45 @@
 # student data function example
 
 # load the student data:
-s=read.csv("students.csv",header=TRUE,row.names=1)
+s=read.csv("students-example.csv",header=TRUE,row.names=1)
 
 # those heights that are reported in feet
-summary(s$height[s$htLab=="ft"])
+summary(s$Height[s$Units=="in"])
 
 # a boxplot:
-boxplot(s$height[s$htLab=="ft"],main="Heights of Students", ylab="feet", col="blue")
+boxplot(s$Height[s$Units=="in"],main="Heights of Students", ylab="in", col="blue")
 
 # ----------  writing a simple function to convert heights ----------
 
 # define constants for the conversions
-ft.cm.Factor = 30.48 # feet to cm
-ft.b.Factor = 0.2 # feet to Bhaskars
+in.cm.Factor = 2.54 # feet to cm
 
 # perform height conversion 
 # input is assumed to be a single value
 # input value assumed to be in feet
 # (or conversion will be incorrect)
-htConvertSimple = function(f) {
-  ft.cm.Factor = 30.48
-  ft.b.Factor = 0.1
-  
-  # find equivalent measurement in Bhaskars
-  bh = f * ft.b.Factor
+htConvertSimple = function(inch) {
+  in.cm.Factor = 2.54
+
   # find equivalent measurement in cm
-  ch = f * ft.cm.Factor
+  cm = inch * in.cm.Factor
   
-  # create vector of the 3 values and return it
-  return ( c(f, ch, bh) )
+  # create vector of the 2 values and return it
+  return ( c(inch, cm) )
 }
 
 students = s
 
-# call on a single height in feet
-htConvertSimple(students$height[1])
+# call on a single height in inches
+htConvertSimple(students$Height[1])
 
 # what happens when we call on all heights?
-htConvertSimple(students$height)
+htConvertSimple(students$Height)
 
 # call all using sapply
-sapply(students$height[students$htLab=="ft"],htConvertSimple)
+sapply(students$Height[students$Units=="in"],htConvertSimple)
 # transpose to make the layout look more familiar?
-res=t(sapply(students$height[students$htLab=="ft"],htConvertSimple))
+res=t(sapply(students$Height[students$Units=="in"],htConvertSimple))
 
 # put the result in a data frame:
-heights = data.frame(feet=res[,1], cm=res[,2], bh=res[,3])
+heights = data.frame(inches=res[,1], cm=res[,2])
